6be3d2556fe1be3d492e0b899f0c56d24b6f2cd5
Robert Rumney <robertrumney@gmail.com>
2023-05-21 12:08:48 +0200

    Update MusicManager.cpp

--- Code/MusicManager.cpp
+++ Code/MusicManager.cpp
@@ -1,12 +1,14 @@
+// Copyright 2023 Robert Rumney Unreal Engine C++ 48 Hour Game-Jam
+
 #include "MusicManager.h"
 #include "Sound/SoundBase.h"
 #include "Components/AudioComponent.h"
 
 AMusicManager::AMusicManager()
 {
-    // Create the audio component
-    AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
-    RootComponent = AudioComponent;
+    // Create the audio components
+    MusicAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("MusicAudioComponent"));
+    MonologueAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("MonologueAudioComponent"));
 
     // Set default values
     bLoopMusic = true;
@@ -18,8 +20,12 @@ void AMusicManager::BeginPlay()
 {
     Super::BeginPlay();
 
-    // Play the music
+    // Play the music and monologue
     PlayMusic();
+
+    // Set a timer to call PlayMonologue after a delay
+    FTimerHandle MonologueTimerHandle;
+    GetWorld()->GetTimerManager().SetTimer(MonologueTimerHandle, this, &AMusicManager::PlayMonologue, 2.0f, false);
 }
 
 void AMusicManager::PlayMusic()
@@ -27,32 +33,41 @@ void AMusicManager::PlayMusic()
     if (MusicSound != nullptr)
     {
         // Set music sound and loop mode
-        AudioComponent->SetSound(MusicSound);
-        AudioComponent->SetLooping(bLoopMusic);
+        MusicAudioComponent->SetSound(MusicSound);
+
+        // Set music looping
+        if (MusicSound->IsA<USoundWave>())
+        {
+            USoundWave* SoundWave = Cast<USoundWave>(MusicSound);
+            SoundWave->bLooping = bLoopMusic;
+        }
 
         // Set music volume
-        AudioComponent->SetVolumeMultiplier(MusicVolume);
+        MusicAudioComponent->SetVolumeMultiplier(MusicVolume);
 
         // Play the music
-        AudioComponent->Play();
+        MusicAudioComponent->Play();
     }
 }
 
-void AMusicManager::PlayMonologue(USoundBase* MonologueSound)
+void AMusicManager::PlayMonologue()
 {
     if (MonologueSound != nullptr)
     {
-        // Stop any currently playing monologue
-        AudioComponent->Stop();
-
         // Set monologue sound and loop mode
-        AudioComponent->SetSound(MonologueSound);
-        AudioComponent->SetLooping(false);
+        MonologueAudioComponent->SetSound(MonologueSound);
+
+        // Set monolgue looping
+        if (MonologueSound->IsA<USoundWave>())
+        {
+            USoundWave* SoundWave = Cast<USoundWave>(MonologueSound);
+            SoundWave->bLooping = bLoopMusic; // Remember to change this if you want a different looping behavior for the monologue
+        }
 
         // Set monologue volume
-        AudioComponent->SetVolumeMultiplier(MonologueVolume);
+        MonologueAudioComponent->SetVolumeMultiplier(MonologueVolume);
 
         // Play the monologue
-        AudioComponent->Play();
+        MonologueAudioComponent->Play();
     }
 }
