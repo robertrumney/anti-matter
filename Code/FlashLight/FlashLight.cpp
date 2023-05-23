d66cdb439a4c8af47be45d52f83c8bcbcb168a27
Robert Rumney <robertrumney@gmail.com>
2023-05-23 03:03:05 +0200

    Update FlashLight.cpp

--- Code/FlashLight/FlashLight.cpp
+++ Code/FlashLight/FlashLight.cpp
@@ -6,7 +6,6 @@
 #include "Components/SpotLightComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
-#include "UObject/SoftObjectPtr.h"
 
 AFlashLight::AFlashLight()
 {
@@ -16,24 +15,6 @@ AFlashLight::AFlashLight()
 
 	SpotLight = CreateDefaultSubobject<USpotLightComponent>(TEXT("SpotLight"));
 	SpotLight->SetupAttachment(RootComponent);
-
-	// Folder path of the mesh asset in the Content Browser
-	FString MeshFolderPath = "/AntiMatterGame/Content/SketchFab-Models/Torch/textures/"; 
-
-	// Name of the mesh asset file (without extension)
-	FString MeshAssetName = "model__1__midComb_light_bottom";
-
-	// Construct the full asset reference path
-	FString MeshAssetPath = MeshFolderPath + MeshAssetName;
-
-	// Create a Soft Object Ptr using the asset reference path
-	FSoftObjectPath SoftObjectPath(MeshAssetPath);
-
-	// Resolve the Soft Object Ptr to get the actual mesh object
-	UStaticMesh* MeshAsset = Cast<UStaticMesh>(SoftObjectPath.ResolveObject());
-
-	FlashLightMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FlashlightMesh"));
-	FlashLightMesh->SetupAttachment(RootComponent);
 }
 
 void AFlashLight::ToggleFlashlight()
@@ -55,10 +36,6 @@ void AFlashLight::BeginPlay()
 			AttachToComponent(PlayerCharacter->GetRootComponent(), FAttachmentTransformRules::SnapToTargetIncludingScale);
 		}
 	}
-
-	// Set the relative location and rotation of the flashlight mesh
-	FlashLightMesh->SetRelativeLocation(FVector(100.0f, 0.0f, 0.0f));
-	FlashLightMesh->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
 }
 
 void AFlashLight::Tick(float DeltaTime)
